30061feb2538ad7b461925b2cb03649f2352223e
praveenrkk <praveenceoa@gmail.com>
2023-02-06 11:48:40 +0530

    initial commit

---
+++ create.js
@@ -0,0 +1,290 @@
+const tagActiveClass = 'tagActive';
+const searchHiddenClass = 'searchHidden';
+const closeTagClass = 'closeTag';
+const tagClass = 'tag';
+
+const jobsListings = [
+  {
+    "id": 1,
+    "company": "Photosnap",
+    "logo": "./images/photosnap.svg",
+    "new": true,
+    "featured": true,
+    "position": "Senior Frontend Developer",
+    "role": "Frontend",
+    "level": "Senior",
+    "postedAt": "1d ago",
+    "contract": "Full Time",
+    "location": "USA Only",
+    "languages": ["HTML", "CSS", "JavaScript"]
+  },
+  {
+    "id": 2,
+    "company": "Manage",
+    "logo": "./images/manage.svg",
+    "new": true,
+    "featured": true,
+    "position": "Fullstack Developer",
+    "role": "Fullstack",
+    "level": "Midweight",
+    "postedAt": "1d ago",
+    "contract": "Part Time",
+    "location": "Remote",
+    "languages": ["Python"],
+    "tools": ["React"]
+  },
+  {
+    "id": 3,
+    "company": "Account",
+    "logo": "./images/account.svg",
+    "new": true,
+    "featured": false,
+    "position": "Junior Frontend Developer",
+    "role": "Frontend",
+    "level": "Junior",
+    "postedAt": "2d ago",
+    "contract": "Part Time",
+    "location": "USA Only",
+    "languages": ["JavaScript"],
+    "tools": ["React", "Sass"]
+  },
+  {
+    "id": 4,
+    "company": "MyHome",
+    "logo": "./images/myhome.svg",
+    "new": false,
+    "featured": false,
+    "position": "Junior Frontend Developer",
+    "role": "Frontend",
+    "level": "Junior",
+    "postedAt": "5d ago",
+    "contract": "Contract",
+    "location": "USA Only",
+    "languages": ["CSS", "JavaScript"]
+  },
+  {
+    "id": 5,
+    "company": "Loop Studios",
+    "logo": "./images/loop-studios.svg",
+    "new": false,
+    "featured": false,
+    "position": "Software Engineer",
+    "role": "FullStack",
+    "level": "Midweight",
+    "postedAt": "1w ago",
+    "contract": "Full Time",
+    "location": "Worldwide",
+    "languages": ["JavaScript"],
+    "tools": ["Ruby", "Sass"]
+  },
+  {
+    "id": 6,
+    "company": "FaceIt",
+    "logo": "./images/faceit.svg",
+    "new": false,
+    "featured": false,
+    "position": "Junior Backend Developer",
+    "role": "Backend",
+    "level": "Junior",
+    "postedAt": "2w ago",
+    "contract": "Full Time",
+    "location": "UK Only",
+    "tools": ["RoR"]
+  },
+  {
+    "id": 7,
+    "company": "Shortly",
+    "logo": "./images/shortly.svg",
+    "new": false,
+    "featured": false,
+    "position": "Junior Developer",
+    "role": "Frontend",
+    "level": "Junior",
+    "postedAt": "2w ago",
+    "contract": "Full Time",
+    "location": "Worldwide",
+    "languages": ["HTML", "JavaScript"],
+    "tools": ["Sass"]
+  },
+  {
+    "id": 8,
+    "company": "Insure",
+    "logo": "./images/insure.svg",
+    "new": false,
+    "featured": false,
+    "position": "Junior Frontend Developer",
+    "role": "Frontend",
+    "level": "Junior",
+    "postedAt": "2w ago",
+    "contract": "Full Time",
+    "location": "USA Only",
+    "languages": ["JavaScript"],
+    "tools": ["Vue, Sass"]
+  },
+  {
+    "id": 9,
+    "company": "Eyecam Co.",
+    "logo": "./images/eyecam-co.svg",
+    "new": false,
+    "featured": false,
+    "position": "Full Stack Engineer",
+    "role": "Fullstack",
+    "level": "Midweight",
+    "postedAt": "3w ago",
+    "contract": "Full Time",
+    "location": "Worldwide",
+    "languages": ["JavaScript", "Python"],
+    "tools": ["Django"]
+  },
+  {
+    "id": 10,
+    "company": "The Air Filter Company",
+    "logo": "./images/the-air-filter-company.svg",
+    "new": false,
+    "featured": false,
+    "position": "Front-end Dev",
+    "role": "Frontend",
+    "level": "Junior",
+    "postedAt": "1mo ago",
+    "contract": "Part Time",
+    "location": "Worldwide",
+    "languages": ["JavaScript"],
+    "tools": ["React", "Sass"]
+  }
+];
+
+function getTag(tag, tagClasses) {
+    return `<span class="${tagClasses}">
+                ${tag}
+            </span>`;
+}
+
+function getJobListing(jobData, filterTags = []) {
+    const jobTagPlaceholder = '###JOB_TAGS###';
+    let jobListingHTML = `
+        <div class="jobsItem">
+            <div class="jobsColumn jobsColumnLeft">
+                <img src="${jobData.logo}" alt="${jobData.company}" class="jobsImg" />
+                <div class="jobsInfo">
+                    <span class="jobsCompany">${jobData.company}</span>
+                    <span class="jobsTitle">${jobData.position}</span>
+                    
+                    <ul class="jobsDetails">
+                        <li class="jobsDetailsItem">${jobData.postedAt}</li>
+                        <li class="jobsDetailsItem">${jobData.contract}</li>
+                        <li class="jobsDetailsItem">${jobData.location}</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="jobsColumn jobsColumnRight">
+                ${jobTagPlaceholder}
+            </div>
+        </div>
+    `;
+
+    const tagsList = [
+        jobData.role,
+        jobData.level,
+        ...(jobData.languages || []),
+        ...(jobData.tools || [])
+    ];
+    const tagsListLowercase = tagsList.map(e => e && e.toLowerCase());
+    const passesFilter = !filterTags.length || filterTags.every(tag => (
+        tagsListLowercase.includes(tag && tag.toLowerCase())
+    ));
+    
+    if (!passesFilter) {
+        return '';
+    }
+
+    const tagsString = tagsList.reduce((acc, currentTag) => {
+        const activeClass = (filterTags.includes(currentTag) && tagActiveClass) || '';
+
+        return acc + getTag(currentTag, `${tagClass} ${activeClass}`);
+    }, '');
+
+    return jobListingHTML.replace(jobTagPlaceholder, tagsString);
+};
+
+function toggleClass(el, className) {
+    if (el.classList.contains(className)) {
+        el.classList.remove(className);
+
+        return;
+    }
+    
+    el.classList.add(className);
+}
+
+function getSearchBar(tagValue, searchContentEl) {
+    let searchBarTags = Array.from(searchContentEl.children)
+        .map(node => node.innerHTML && node.innerHTML.trim())
+        .filter(tag => !!tag);
+
+    if (searchBarTags.includes(tagValue)) {
+        searchBarTags = searchBarTags.filter(tag => tag !== tagValue);
+    } else {
+        // searchBarTags = [...searchBarTags, tagValue];
+        searchBarTags.push(tagValue)
+    }
+
+    return searchBarTags;
+}
+
+function setJobsListings(filterTags) {
+    const jobsListingsHTML = jobsListings.reduce((acc, currentListing) => {
+        return acc + getJobListing(currentListing, filterTags);
+    }, '');
+    
+    document.getElementById('jobs').innerHTML = jobsListingsHTML;
+}
+
+function displaySearchWrapper(display = false) {
+    const searchWrapper = document.getElementById('search');
+    
+    if (display) {
+        searchWrapper.classList.remove(searchHiddenClass);
+
+        return;
+    }
+
+    searchWrapper.classList.add(searchHiddenClass);
+}
+
+function setSearchbarContent(searchContentEl, tags) {
+    searchContentEl.innerHTML = tags.reduce((acc, currentTag) => {
+        return acc + getTag(currentTag, closeTagClass);
+    }, '');
+}
+
+function reset(searchContentEl) {
+    searchContentEl.innerHTML = '';
+
+    setJobsListings();
+    displaySearchWrapper(false);
+    toggleClass(targetEl, tagActiveClass);
+}
+
+window.addEventListener('click', (event) => {
+    const targetEl = event.target;
+    const targetText = targetEl.innerHTML.trim();
+    const searchContentEl = document.getElementById('searchContent');
+    const searchBarTags = getSearchBar(targetText, searchContentEl);
+
+    if (targetEl.id === 'clear' || !searchBarTags.length) {
+        reset(searchContentEl);
+
+        // return;
+    }
+
+    if (![tagClass, closeTagClass].some(c => targetEl.classList.contains(c))) {
+        return;
+    }
+
+    setSearchbarContent(searchContentEl, searchBarTags);
+    toggleClass(targetEl, tagActiveClass);
+    displaySearchWrapper(searchBarTags.length > 0);
+    setJobsListings(searchBarTags);
+});
+
+setJobsListings();
